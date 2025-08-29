ccbb9736fb5715529cd774ed5f2a2bc32eaeb7b2
ml2me <8888gromov@gmail.com>
2025-08-29 20:21:15 +0300

    Update bubble_sort.py
    Добавлены комментарии касательно оптимизации

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,18 +1,19 @@
 #  Пузырьковая сортировка (по неубыванию)
-#  Для сортировки по невозрастанию - поменять знак на <
+#  Для сортировки по невозрастанию - поменять знак на "<"
 
 def bubble_sort(lst):
     n = len(lst)
     for i in range(n-1):
         srtd = False
         for j in range(n-i-1):
-            if lst[j] > lst[j+1]:  #  Для сортировки по невозрастанию - поменять знак на <
+            if lst[j] > lst[j+1]:  #  Для сортировки по невозрастанию - поменять знак на "<"
                 lst[j], lst[j+1] = lst[j+1], lst[j]
                 srtd = True
-        if not srtd:
+        if not srtd:  #  если элементы отсортированы
             break
 
 # Пример
 nums = [3, 4, 1, 2, 5]
 bubble_sort(nums)
+
 print(nums)
