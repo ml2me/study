8796169b7ed52b86ba5327ea4b1dd95974efedf2
ml2me <8888gromov@gmail.com>
2026-03-13 12:45:32 +0300

    ODS NLP 4

---
+++ NLP/4/w2v.py
@@ -0,0 +1,27 @@
+import random
+
+RANDOM_STATE = 42
+
+def gv_spliter(text2tok):  #  tokenizer
+    return text2tok.split()
+
+def init_random_vec(tok_list2vec, vec_dimension):
+    token_vectors = {}
+    for token in tok_list2vec:
+        token_vectors[token] = [random.uniform(0, 1) for _ in range(vec_dimension)]
+    return token_vectors
+
+def train(data: str):
+    """
+    return: w2v_dict: dict
+            - key: string (word)
+            - value: np.array (embedding)
+    """
+    return init_random_vec(gv_spliter(data), 2)
+	
+gv_clean_text = 'эта функция часто используется в nlp обработке естественного языка для предварительной подготовки текста перед анализом или векторизацией'
+
+p = train(gv_clean_text)
+
+for k,v in p.items():
+    print(f"{k}: {v}")
